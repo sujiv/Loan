456bd95ed0888164453d24a6dcf3817128dfece4
aman <azewgee@gmail.com>
2020-04-08 19:22:51 -0400

    after inserting both images and basic data

--- src/main/java/com/altimterik/Loan/repository/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.altimterik.Loan.repository;
-//
-//import com.altimterik.Loan.models.LoanBasicData;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface LoanRepository extends JpaRepository<LoanBasicData, Long> {
-//}
