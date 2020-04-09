3fb7879ed14b6916d312c63d3304a1ff757d326a
aman <azewgee@gmail.com>
2020-04-09 08:13:39 -0400

    after inserting both images and basic data

--- src/main/java/com/altimterik/Loan/repository/FileStorageRepo.java
+++ src/main/java/com/altimterik/Loan/repository/FileStorageRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FileStorageRepo extends JpaRepository<FileStorage, Integer> {
+public interface FileStorageRepo extends JpaRepository<FileStorage,Integer> {
 }
